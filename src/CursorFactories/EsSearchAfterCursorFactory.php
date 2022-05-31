28237e8e3b19fdf93dee9e70705281616fea676c
Niklas Ekman <niklas@stockfiller.com>
2022-05-31 15:59:31 +0200

    fix

--- src/CursorFactories/EsSearchAfterCursorFactory.php
+++ src/CursorFactories/EsSearchAfterCursorFactory.php
@@ -9,29 +9,19 @@ class EsSearchAfterCursorFactory extends BaseCursorFactory
 {
     private Client $client;
     private int $pageSize;
-    private array $defaultSort;
 
     public function __construct(
         Client $client,
         int $pageSize = EsUtility::DEFAULT_PAGE_SIZE,
-        array $defaultSort = [["_id" => "asc"]]
     ) {
         $this->client = $client;
         $this->pageSize = $pageSize;
-        $this->defaultSort = $defaultSort;
     }
 
     public function responses(array $params = []): iterable
     {
         if (!isset($params["size"])) {
             $params["size"] = $this->pageSize;
-        }
-
-        // If no sorting was provided in the initial params, the last hit will have no sorting as well. Therefore,
-        // we need to fallback to something that is similar to all entities which is an "_id". Note that ES
-        // does not recommend using "_id" here, not much to do though.
-        if (empty($params["body"]["sort"])) {
-            $params = EsUtility::paramsSort($params, $this->defaultSort);
         }
 
         yield $response = $this->client->search($params);
