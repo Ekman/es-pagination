b486be0dfe76fbf5bbd774b6ad11e1214aa0f0c5
Niklas Ekman <niklas@stockfiller.com>
2022-06-09 15:53:59 +0200

    Hint exceptions

--- src/Contracts/EsCursorFactoryInterface.php
+++ src/Contracts/EsCursorFactoryInterface.php
@@ -8,6 +8,7 @@ interface EsCursorFactoryInterface
      * Paginate an ES search by yielding each response
      * @param array $params ES search parameters
      * @return iterable Each response
+	 * @throws EsPaginationExceptionInterface
      */
     public function responses(array $params = []): iterable;
 
@@ -15,6 +16,7 @@ interface EsCursorFactoryInterface
      * Paginate an ES search by yielding each hit within a response
      * @param array $params ES search parameters
      * @return iterable Each hit within each response
+	 * @throws EsPaginationExceptionInterface
      */
     public function hits(array $params = []): iterable;
 }
